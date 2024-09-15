ff9bbd59513544b810f97eaf85840fd108ec74bf
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-15 12:47:15 +0300

    updating products

--- src/components/products/product-items.js
+++ src/components/products/product-items.js
@@ -1,8 +1,12 @@
 export default function ProductItem(){
     return(
                 <div className="bg-blue-50 p-4 rounded-md text-center
-                hover:bg-blue-100">
-                    <img src="/skin-care.png" alt="Skincare"/>
+                hover:bg-blue-100 hover:shadow-2xl hover:shadow-gray transition-all">
+                    <div className="text-center">
+                    <img src="/skin-care.png" 
+                    className="max-h-auto max-h-24 block mx-auto"
+                     alt="Skincare"/>
+                    </div>
                     <h4 className="font-semibold text-xl my-3">Skin Care</h4>
                     <p className="text-gray-500 text-sm">Keep your skin healthy and glowing with our 
                         expert skincare products. Hydrate, protect, 
