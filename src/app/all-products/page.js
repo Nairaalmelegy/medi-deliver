718c5603dbf2bd0787c7e7caf40d80d58d41e6bf
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-20 23:20:17 +0300

    commiting all products page

--- src/app/all-products/page.js
+++ src/app/all-products/page.js
@@ -3,17 +3,24 @@ import SectionHeaders from "@/components/layout/sectionHeaders";
 import ProductItem from "@/components/products/product-items";
 import {useEffect, useState} from "react";
 
+
+// MenuPage Component: Displays product categories and associated items
 export default function MenuPage() {
   const [categories, setCategories] = useState([]);
   const [menuItems, setMenuItems] = useState([]);
+  
+  
+  // useEffect hook to fetch data on component mount
   useEffect(() => {
+    // Fetch categories from the API and update the state
     fetch('/api/categories').then(res => {
       res.json().then(categories => setCategories(categories))
     });
+    // Fetch products from the API and update the state
     fetch('/api/products').then(res => {
       res.json().then(menuItems => setMenuItems(menuItems));
     });
-  }, []);
+  }, []); // Empty dependency array ensures this only runs once when the component is mounted
   return (
     <section className="mt-8">
       {categories?.length > 0 && categories.map(c => (
