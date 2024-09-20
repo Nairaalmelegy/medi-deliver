c6656d386f8b4ad8580b3669c640a722336740d4
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-20 21:21:15 +0300

    adding to cart

--- src/components/products/product-items.js
+++ src/components/products/product-items.js
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import { CartContext } from "../AppContext";
+import { useState } from "react";
 
-export default function ProductItem({ name, description, image, price }) {
+export default function ProductItem(menuItem) {
+  const {
+    name, description, image, price,
+  } = menuItem;
+
+  function handleAddToCartButtonClick(){
+    addToCart(menuItem);
+  }
+  const [showPopup,  setShowPopup] = useState(false);
+
+  const {addToCart} = useContext(CartContext);
   return (
     <div className="bg-blue-50 p-4 rounded-md text-center hover:bg-blue-100 hover:shadow-2xl hover:shadow-gray transition-all flex flex-col justify-between h-full">
       <div>
@@ -9,7 +22,10 @@ export default function ProductItem({ name, description, image, price }) {
         <h4 className="font-semibold text-xl my-3">{name}</h4>
         <p className="text-gray-500 text-sm">{description}</p>
       </div>
-      <button className="bg-primary mt-4 text-white rounded-full px-8 py-2">
+      <button
+      type="button" 
+      onClick={handleAddToCartButtonClick}
+      className="bg-primary mt-4 text-white rounded-full px-8 py-2">
         Add to cart ${price} {/* Display price dynamically */}
       </button>
     </div>
